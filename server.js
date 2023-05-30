b9deb06f4661fa98e9a5e325ec80415af7c223db
Dan Rashid <git@danrashid.com>
2023-05-29 19:39:55 -0500

    Make server better

--- server.js
+++ server.js
@@ -3,54 +3,66 @@ const https = require("node:https");
 
 const client_secret = process.env.GITHUB_CLIENT_SECRET;
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
+const handleMessage = (message, handleEnd, handleError) => {
   let rawData = "";
 
-  req.on("data", (chunk) => {
+  message.on("data", (chunk) => {
     rawData += chunk;
   });
 
-  req.on("end", () => {
-    const ghreq = https.request(
-      "https://github.com/login/oauth/access_token",
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-        },
-      },
-      (ghres) => {
-        let ghRawData = "";
-
-        ghres.on("data", (chunk) => {
-          ghRawData += chunk;
-        });
-
-        ghres.on("end", () => {
-          res.end(ghRawData);
-        });
-
-        ghres.on("error", (err) => {
-          console.error(err);
-          ghres.end();
-        });
-      }
-    );
-
-    ghreq.on("error", (err) => {
-      console.error(err);
-      res.end();
-    });
-
-    ghreq.end(`client_secret=${client_secret}&${rawData}`);
+  message.on("end", () => {
+    handleEnd(rawData);
   });
 
-  req.on("error", (err) => {
+  message.on("error", (err) => {
     console.error(err);
-    res.end();
+    handleError(err);
   });
+};
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  handleMessage(
+    req,
+    (rawData) => {
+      const ghreq = https.request(
+        "https://github.com/login/oauth/access_token",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+          },
+        },
+        (ghres) => {
+          let ghRawData = "";
+
+          ghres.on("data", (chunk) => {
+            ghRawData += chunk;
+          });
+
+          ghres.on("end", () => {
+            res.end(ghRawData);
+          });
+
+          ghres.on("error", (err) => {
+            console.error(err);
+            ghres.end();
+          });
+        }
+      );
+
+      ghreq.on("error", (err) => {
+        console.error(err);
+        res.end();
+      });
+
+      ghreq.end(`client_secret=${client_secret}&${rawData}`);
+    },
+    (err) => {
+      res.end(err);
+    }
+  );
 });
 
 server.listen(9900);
