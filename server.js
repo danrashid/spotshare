506a70c0bf5bcbcf23864a097989280df875fda8
Dan Rashid <git@danrashid.com>
2023-05-30 18:08:40 -0500

    Add routing

--- server.js
+++ server.js
@@ -23,41 +23,49 @@ const handleMessage = (message, handleEnd, handleError) => {
 const server = http.createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
-  handleMessage(
-    req,
-    (rawData) => {
-      const ghreq = https.request(
-        "https://github.com/login/oauth/access_token",
-        {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-          },
-        },
-        (ghres) => {
-          handleMessage(
-            ghres,
-            (ghRawData) => {
-              res.end(ghRawData);
+  switch (req.url) {
+    case "/github_access_token":
+      handleMessage(
+        req,
+        (rawData) => {
+          const ghreq = https.request(
+            "https://github.com/login/oauth/access_token",
+            {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+              },
             },
-            (ghErr) => {
-              ghres.end(ghErr);
+            (ghres) => {
+              handleMessage(
+                ghres,
+                (ghRawData) => {
+                  res.end(ghRawData);
+                },
+                (ghErr) => {
+                  ghres.end(ghErr);
+                }
+              );
             }
           );
+
+          ghreq.on("error", (err) => {
+            console.error(err);
+            res.end();
+          });
+
+          ghreq.end(`client_secret=${client_secret}&${rawData}`);
+        },
+        (err) => {
+          res.end(err);
         }
       );
-
-      ghreq.on("error", (err) => {
-        console.error(err);
-        res.end();
-      });
-
-      ghreq.end(`client_secret=${client_secret}&${rawData}`);
-    },
-    (err) => {
-      res.end(err);
-    }
-  );
+      break;
+    case "":
+      break;
+    default:
+      res.end("404");
+  }
 });
 
 server.listen(9900);
